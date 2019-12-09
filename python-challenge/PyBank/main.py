332a37f2a3aa9663a4badca4966431bac14bb8c4
onyemma0312 <56767993+onyemma0312@users.noreply.github.com>
2019-12-08 19:03:06 -0500

    Update main.py

--- python-challenge/PyBank/main.py
+++ python-challenge/PyBank/main.py
@@ -1,21 +1,50 @@
 import os
 import csv
-count = 0
-total = []
-with open('budget_data.csv', 'r') as csv_file:
-    csv_reader = csv.reader(csv_file)
-    for row in csv_reader:
+csvpath = os.path.join('.', 'Resources', 'budget_data.csv')
+outpath = os.path.join('.', "Resources", "output_analysis.txt")
+count=0
+plchange = 0
+pl = []
+previouspl = pl
+sum_pl_change = 0
+greatestincrease = ["", 0]
+greatestdecrease = ["", 999999]
+
+with open(csvpath, 'r') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+    next(csvreader)
+    rowtotal = 0
+    for row in csvreader:
+        pl.append(row[1])
+   #The total number of months included in the datase
         count = count + 1
-print('The total number of months included in the dataset', (count-1))
-The total number of months included in the dataset 86
-with open('budget_data.csv', 'r') as csv_file:
-    csv_reader = csv.reader(csv_file)
-    for row in csv_reader:
-        total.append(int(row[1]))
-        total_revenue += total[row]
-        print(total_revenue)
-#calculate average_change
-        average_change = round(total_revenue/count, 2)
+   #The net total amount of "Profit/Losses" over the entire period
+        rowtotal += int(row[1])
+    #The average of the changes in "Profit/Losses" over the entire period
+        average_pl_change = round(rowtotal/count, 2)
+        
+      
+    #Greatest Increase
+        
+    #Greatest Decrease
+      
+    
+#print the outcomes
+output = (
+    f"Total Months: {count}\n"
+    f"Total Revenue: ${rowtotal}\n"
+    f"Average Revenue Change: ${average_pl_change}\n"
+    f"Greatest increase in profits: {greatestincrease[0]} ${greatestincrease[1]}\n"
+    f"Greatest decrease in profits: {greatestdecrease[0]} ${greatestdecrease[1]}\n"
+
+)
+
+print(output)
+
+#Write to the text path
+with open(outpath, "w") as txt_file:
+    txt_file.write(output)
+        
         
     
 
